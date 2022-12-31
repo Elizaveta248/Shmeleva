264e7db783d35bbf3a1ee28f49b9fa853edf86a8
Елизавета <Elizaveta.Shmeleva@urfu.me>
2022-12-31 19:15:34 +0500

    Коммит 2.3.1

---
+++ Report.py
@@ -0,0 +1,246 @@
+from statistics import mean
+import matplotlib.pyplot as plt
+import numpy as np
+from openpyxl import Workbook
+from openpyxl.styles import Border, Font, Side, Alignment
+from jinja2 import Environment, FileSystemLoader
+import pdfkit
+
+
+class Statistics:
+    """Класс, представляющий методы для статистического анализа данных
+    Attributes:
+        nameProfession (str): Название профессии, которую нужно проанализировать
+        salaryLevel (dict): Динамика уровня зарплат по годам
+        numberVacancies (dict): Динамика количества вакансий по годам
+        selectedSalaryLevel (dict): Динамика уровня зарплат по годам для выбранной профессии
+        selectedNumberVacancies (dict): Динамика количества вакансий по годам для выбранной профессии
+        salariesСity (dict): Уровень зарплат по городам
+        vacanciesСity (dict): Доля вакансий по городам
+        counts (int): Счётчик вакансий
+        years (list): Список представленных лет
+    """
+
+    def __init__(self):
+        """Инициализирует объект Statistics"""
+        self.nameProfession = str(input("Введите название профессии: "))
+        self.salaryLevel = {}
+        self.numberVacancies = {}
+        self.selectedSalaryLevel = {}
+        self.selectedNumberVacancies = {}
+        self.salariesСity = {}
+        self.vacanciesСity = {}
+        self.counts = 0
+        self.years = []
+
+    def filtering(self, vacancy: dict):
+        """Анализирует данные вакансии, обновляет счетчики в словарях.
+        С помощью словаря currency_to_rub переводит зарплату в рубли.
+        Args:
+            vacancy (dict): Анализируемая вакансия
+        """
+        year = int(vacancy['published_at'][0:4])
+        self.counts += 1
+        if year not in self.years:
+            self.years.append(year)
+        salary = int(mean((float(vacancy['salary_from']), float(vacancy['salary_to']))) * currency_to_rub[
+            vacancy['salary_currency']])
+        self.numberVacancies.update({year: self.numberVacancies.get(year, 0) + 1})
+        self.salaryLevel.update({year: self.salaryLevel.get(year, []) + [salary]})
+        if self.nameProfession in vacancy['name']:
+            self.selectedNumberVacancies.update({year: self.selectedNumberVacancies.get(year, 0) + 1})
+            self.selectedSalaryLevel.update({year: self.selectedSalaryLevel.get(year, []) + [salary]})
+        self.vacanciesСity.update({vacancy['area_name']: self.vacanciesСity.get(vacancy['area_name'], 0) + 1})
+        self.salariesСity.update({vacancy['area_name']: self.salariesСity.get(vacancy['area_name'], []) + [salary]})
+        if len(self.selectedNumberVacancies) == 0:
+            self.selectedNumberVacancies.update({year: 0})
+            self.selectedSalaryLevel.update({year: []})
+
+    def print_vacancies(self):
+        """Производит вывод полученных данных в консоль
+        Returns:
+            dict: Список полученных данных
+        """
+        salaryYear = {key: int(mean(value)) for key, value in self.salaryLevel.items()}
+        numberVacancies = self.numberVacancies
+        selectedSalaryYear = {key: (int(mean(value)) if len(value) > 0 else 0) for key, value in
+                              self.selectedSalaryLevel.items()}
+        selectedNumberVacancies = self.selectedNumberVacancies
+        salaryCity = dict(sorted({key: int(mean(value)) for key, value in self.salariesСity.items()}.items(),
+                                 key=lambda item: item[1], reverse=True)[0:10])
+        vacanciesCity = dict(
+            sorted({key: round(value / self.counts, 4) for key, value in self.vacanciesСity.items()}.items(),
+                   key=lambda item: item[1], reverse=True)[0:10])
+
+        print('Динамика уровня зарплат по годам:', salaryYear)
+        print('Динамика количества вакансий по годам:', numberVacancies)
+        print('Динамика уровня зарплат по годам для выбранной профессии:', selectedSalaryYear)
+        print('Динамика количества вакансий по годам для выбранной профессии:', selectedNumberVacancies)
+        print('Уровень зарплат по городам (в порядке убывания):', salaryCity)
+        print('Доля вакансий по городам (в порядке убывания):', vacanciesCity)
+
+        return {'salaryYear': salaryYear, 'numberVacancies': numberVacancies,
+                'selectedSalaryYear': selectedSalaryYear, 'selectedNumberVacancies': selectedNumberVacancies,
+                'salaryCity': salaryCity, 'vacanciesCity': vacanciesCity,
+                'years': self.years}
+
+
+class Report:
+    """Класс, генерирующий отчёт
+    Attributes:
+        nameProfession (str): Название профессии, которую нужно проанализировать
+    """
+
+    def __init__(self, nameProfession):
+        """Инициализирует объект Report
+        Args:
+            nameProfession (str): Название профессии, которую нужно проанализировать
+        """
+        self.nameProfession = nameProfession
+
+    def generate_excel(self, dataList: list):
+        """Генерирует Excel-файл (таблицу) со статистикой
+        Args:
+            dataList (list): Список с данными о собранной статистике
+        """
+        vacanciesBook = Workbook()
+        statisticsYear = vacanciesBook.active
+        statisticsYear.title = "Статистика по годам"
+        statisticsYear.append(
+            ['Год', 'Средняя зарплата', f'Средняя зарплата - {self.nameProfession}', 'Количество вакансий',
+             f'Количество вакансий - {self.nameProfession}'])
+        statisticsCity = vacanciesBook.create_sheet("Статистика по городам")
+        statisticsCity.append(['Город', 'Уровень зарплат', '', 'Город', 'Доля вакансий'])
+        column = ['A', 'B', 'C', 'D', 'E']
+
+        for year in dataList['years']:
+            statisticsYear.append(
+                [year, dataList['salaryYear'][year], dataList['selectedSalaryYear'][year],
+                 dataList['numberVacancies'][year], dataList['selectedNumberVacancies'][year]])
+
+        for key, value in dataList['salaryCity'].items():
+            statisticsCity.append({'A': key, 'B': value})
+        for key, value in dataList['vacanciesCity'].items():
+            statisticsCity.append({'D': key, 'E': "{:.2f}%".format(float(value * 100))})
+
+        statisticsCity.move_range('D12:E21', rows=-10)
+
+        self.styleSetting(statisticsYear, statisticsCity, column)
+        vacanciesBook.save('report.xlsx')
+
+    def styleSetting(self, statisticsYear, statisticsCity, column):
+        """Настраивает внешний вид таблицы
+        Args:
+            statisticsYear (Workbook): Страница в таблице со статистикой по годам
+            statisticsCity (Workbook): Страница в таблице со статистикой по городам
+            column (list): Список столбцов в таблице
+        """
+        font_size = 11
+        ft = Font(bold=True, size=font_size)
+        borders = Border(left=Side(border_style='thin', color='00000000'),
+                         right=Side(border_style='thin', color='00000000'),
+                         top=Side(border_style='thin', color='00000000'),
+                         bottom=Side(border_style='thin', color='00000000'))
+
+        for row in statisticsYear.rows:
+            for cell in row:
+                if cell.value:
+                    cell.border = borders
+        for row in statisticsCity.rows:
+            for cell in row:
+                if cell.value:
+                    cell.border = borders
+
+        for i in column:
+            statisticsYear[f'{i}1'].font, statisticsCity[f'{i}1'].font = ft, ft
+        for i in range(2, 12):
+            statisticsCity[f'E{i}'].alignment = Alignment(horizontal='right')
+
+        self.editWidthColumn(statisticsYear)
+        self.editWidthColumn(statisticsCity)
+
+        statisticsCity.column_dimensions['C'].width = 2
+
+    def editWidthColumn(self, sheet):
+        """Отвечает за автоматическую ширину столбцов таблицы
+        Args:
+            sheet (Workbook): Форматируемая страница в Excel-файле
+        """
+        colsDict = {}
+        for row in sheet.rows:
+            for cell in row:
+                if cell.value:
+                    colsDict[cell.column_letter] = max((colsDict.get(cell.column_letter, 0), len(str(cell.value))))
+        for col, value in colsDict.items():
+            sheet.column_dimensions[col].width = value + 2
+
+    def generate_image(self, dataList: list):
+        """Генерирует изображение со статистическими графиками
+        Args:
+            dataList (list): Список с данными о собранной статистике
+        """
+        x = np.arange(min(dataList['years']), max(dataList['years']) + 1) - 0.25
+
+        plt.rc('font', size=8)
+        width = 0.5
+        plt.subplot(2, 2, 1)
+        plt.title('Уровень зарплат по годам')
+        salaryYear = plt.bar(x, dataList['salaryYear'].values(), width)
+        selectedSalaryYear = plt.bar(x + width, dataList['selectedSalaryYear'].values(), width)
+        plt.grid(axis='y')
+        plt.xticks(dataList['years'], rotation=90, horizontalalignment='center')
+        plt.legend([salaryYear, selectedSalaryYear], ['средняя з/п', f'з/п {self.nameProfession}'])
+
+        plt.subplot(2, 2, 2)
+        plt.title('Количество вакансий по годам')
+        plt.xticks(dataList['years'], rotation=90, horizontalalignment='center')
+        plt.grid(axis='y')
+        numberVacancies = plt.bar(x, dataList['numberVacancies'].values(), width)
+        selectedNumberVacancies = plt.bar(x + width, dataList['selectedNumberVacancies'].values(), width)
+        plt.legend([numberVacancies, selectedNumberVacancies],
+                   ['Количество вакансий', f'Количество вакансий \n {self.nameProfession}'])
+
+        plt.subplot(2, 2, 3)
+        plt.title('Уровень зарплат по городам')
+        salaryCityIndex = [key.replace(" ", "\n").replace("-", "-\n") for key in dataList['salaryCity'].keys()]
+        salaryCityValues = list(dataList['salaryCity'].values())
+        plt.yticks(horizontalalignment='right', verticalalignment="center", fontsize=6)
+        plt.gca().invert_yaxis()
+        plt.barh(salaryCityIndex, salaryCityValues)
+
+        plt.subplot(2, 2, 4)
+        plt.title('Доля вакансий по городам')
+        others = 1 - sum(dataList['vacanciesCity'].values())
+        vacanciesCity = dataList['vacanciesCity'].values() if others == 0 else [others] + list(
+            dataList['vacanciesCity'].values())
+        labels = dataList['vacanciesCity'].keys() if others == 0 else ['Другие'] + list(
+            dataList['vacanciesCity'].keys())
+        plt.pie(vacanciesCity, labels=labels, textprops={"fontsize": 6})
+
+        plt.tight_layout()
+        plt.savefig('graph.png', dpi=300)
+
+    def generate_pdf(self, dataList: list):
+        """Генерирует pfd-отчёт
+        Args:
+            dataList (list): Список с данными о собранной статистике
+        """
+        env = Environment(loader=FileSystemLoader('.'))
+        template = env.get_template("pdf_template.html")
+
+        pdf_template = template.render(
+            {'nameProfessonal': self.nameProfession, 'image_file': 'graph.png', 'dataList': dataList})
+        config = pdfkit.configuration(wkhtmltopdf=r'C:\wkhtmltox\bin\wkhtmltopdf.exe')
+        pdfkit.from_string(pdf_template, 'report.pdf', configuration=config, options={'enable-local-file-access': ""})
+
+
+currency_to_rub = {"AZN": 35.68,
+                   "BYR": 23.91,
+                   "EUR": 59.90,
+                   "GEL": 21.74,
+                   "KGS": 0.76,
+                   "KZT": 0.13,
+                   "RUR": 1,
+                   "UAH": 1.64,
+                   "USD": 60.66,
+                   "UZS": 0.0055, }
---
+++ Table.py
@@ -0,0 +1,305 @@
+import re
+import prettytable
+import datetime
+
+
+class Salary:
+    """Класс для представления зарплаты.
+    Attributes:
+        salary_from (str): Нижняя граница вилки оклада
+        salary_to (str): Верхняя граница вилки оклада
+        salary_gross (str): Оклад указан до вычета налогов
+        salary_currency (str): Валюта оклада
+    """
+
+    def __init__(self, vacancy: dict):
+        """Инициализирует объект Salary, выполняет форматирование для целочисленных полей
+        Args:
+            vacancy (dict): Вакансия
+        """
+        self.salary_from = '{0:,}'.format(int(float(vacancy["Нижняя граница вилки оклада"]))).replace(',', ' ')
+        self.salary_to = '{0:,}'.format(int(float(vacancy["Верхняя граница вилки оклада"]))).replace(',', ' ')
+        self.salary_gross = dictTranslationTaxes[vacancy["Оклад указан до вычета налогов"]]
+        self.salary_currency = dictTranslationСurrency[vacancy["Идентификатор валюты оклада"]]
+
+
+    def convertsToRub(self):
+        """Вычисляет среднюю зарплату и конвертирует её в рубли при помощи словаря - currency_to_rub
+        Returns:
+            float: средняя зарплата в рублях
+        """
+        return currency_to_rub[self.salary_currency] * (
+                    int(self.salary_from.replace(' ', '')) + int(self.salary_to.replace(' ', ''))) / 2
+
+
+class Vacancy:
+    """Класс для представления вакансии.
+    Attributes:
+        name (str): Название вакансии
+        description (str): Описание вакансии
+        key_skills (list): Список навыков
+        experience_id (str): Опыт работы
+        premium (bool or str): Премиум-вакансия
+        employer_name (str): Название компании
+        salary (Salary): Вся информация о зарплате
+        area_name (str): Название региона
+        published_at (datetime): Дата публикации вакансии
+        str_skills (str): Строка навыков
+    """
+
+    def __init__(self, vacancy: dict):
+        """Инициализирует объект Vacancy, выполняет форматирование различных полей с помощью функций и словарей: dictTranslationExperience и dictTranslationBool
+        Args:
+            vacancy (dict): Вакансия
+        """
+        self.name = vacancy['Название']
+        self.description = Vacancy.croppingСharacters(vacancy["Описание"], 100)
+        self.key_skills = re.split("\n", vacancy["Навыки"].replace('&&&&', '\n'))
+        self.experience_id = dictTranslationExperience[vacancy["Опыт работы"]]
+        self.premium = dictTranslationBool[vacancy["Премиум-вакансия"]]
+        self.employer_name = vacancy['Компания']
+        self.salary = Salary(vacancy)
+        self.area_name = vacancy['Название региона']
+        self.published_at = datetime.datetime.strptime(vacancy["Дата публикации вакансии"], "%Y-%m-%dT%H:%M:%S%z")
+        self.str_skills = self.croppingСharacters(vacancy["Навыки"].replace('&&&&', '\n'), 100)
+
+    def croppingСharacters(self, message: str, maxLen: int):
+        """Отвечает за то, чтобы строка была не более заданной длины.
+        Args:
+            message (str): Строка, которую необходимо проверить (при необходимости - обрезать)
+            maxLen (int): Максимальная длина строки
+        Returns:
+            message (str): Обработанная строка
+        """
+        if len(message) >= maxLen:
+            return message[:maxLen] + "..."
+        return message
+
+    def returnsValues(self, parameter):
+        """Возвращает значение атрибута вакансии для сортировки и фильтрации
+
+        Args:
+            parameter (str): Искомый параметр
+        Returns:
+            parameter (str): Искомое значение
+        """
+        heading = {"Название": self.name,
+                   "Описание": self.description,
+                   "Опыт работы": self.experience_id,
+                   "Премиум-вакансия": self.premium,
+                   "Компания": self.employer_name,
+                   "Нижняя граница вилки оклада": self.salary.salary_from,
+                   "Верхняя граница вилки оклада": self.salary.salary_to,
+                   "Оклад указан до вычета налогов": self.salary.salary_gross,
+                   "Идентификатор валюты оклада": self.salary.salary_currency,
+                   "Название региона": self.area_name,
+                   "Дата публикации вакансии": self.published_at}
+        return heading.get(parameter)
+
+
+class Table:
+    """Класс для представления таблицы
+    Attributes:
+        parameterFilter (str): Параметр фильтрации
+        parameterSort (str): Параметр сортировки
+        isReverseSort (str or bool): Обратный порядок сортировки (Да / Нет)
+        borders (list): Диапазон вывода (строки)
+        requiredColumns (list): Требуемые столбцы для вывода
+        isFilter (bool): Нужна ли фильтрация
+        needPrint (bool): Нужно ли печатать
+        vacanciesTable (PrettyTable): Таблица
+    """
+
+    def __init__(self):
+        """Инициализирует объект Table"""
+        self.parameterFilter = str(input("Введите параметр фильтрации: "))
+        self.parameterSort = input("Введите параметр сортировки: ")
+        self.isReverseSort = input("Обратный порядок сортировки (Да / Нет): ")
+        self.borders = list(map(int, input("Введите диапазон вывода: ").split()))
+        self.requiredColumns = list(input("Введите требуемые столбцы: ").split(", "))
+
+    def checkingParameterValues(self):
+        """Проверяет корректность ввода параметра фильтрации
+        Returns:
+            needPrint (bool): Нужно ли печатать
+        """
+        self.isFilter = True
+        self.needPrint = True
+        if self.parameterFilter == '':
+            self.isFilter = False
+        elif ":" not in self.parameterFilter:
+            print("Формат ввода некорректен")
+            self.needPrint = False
+        return self.needPrint
+
+    def checkingEnteredValues(self):
+        """Проверяет валидность вводимых данных
+        Returns:
+            needPrint (bool): Нужно ли печатать
+        """
+        heading = ["№", "Название", "Нижняя граница вилки оклада", "Верхняя граница вилки оклада",
+                   "Оклад указан до вычета налогов", "Описание", "Навыки", "Опыт работы", "Премиум-вакансия",
+                   "Компания", "Оклад", "Название региона", "Дата публикации вакансии", "",
+                   "Идентификатор валюты оклада"]
+        if self.isFilter:
+            self.parameterFilter = list(self.parameterFilter.split(": "))
+            if self.parameterFilter[0] not in heading and self.parameterFilter[0] != "Оклад":
+                print("Параметр поиска некорректен")
+                self.needPrint = False
+        if self.parameterSort not in heading:
+            print("Параметр сортировки некорректен")
+            self.needPrint = False
+        elif self.isReverseSort not in ["Да", "Нет", ""]:
+            print("Порядок сортировки задан некорректно")
+            self.needPrint = False
+        return self.needPrint
+
+    def processingSortOrder(self):
+        """Обрабатывает поле ввода обратной сортировки
+        Returns:
+            isReverseSort (bool or str): Обратный порядок сортировки (Да / Нет)
+        """
+        if self.isReverseSort == "":
+            return False
+        else:
+            return dictTranslationBool[self.isReverseSort]
+
+
+    def sortingVacancies(self, vacancies_list: list):
+        """Сортирует список вакансий
+        Args:
+            vacancies_list (list): Список ваканский, который необходимо отсортировать
+        Returns:
+            vacancies_list (list): Отсортированный список
+        """
+        if self.parameterSort == "Навыки":
+            return vacancies_list.sort(key=lambda vacancy: len(vacancy.key_skills), reverse=self.isReverseSort)
+        if self.parameterSort == "Опыт работы":
+            return vacancies_list.sort(key=lambda vacancy: dictSortionExperience[vacancy.experience_id],
+                                       reverse=self.isReverseSort)
+        if self.parameterSort == "Оклад":
+            return vacancies_list.sort(
+                key=lambda vacancy: vacancy.convertsToRub(), reverse=self.isReverseSort)
+        return vacancies_list.sort(key=lambda vacancy: vacancy.returnsValues(self.parameterSort),
+                                   reverse=self.isReverseSort)
+
+    def formattingBorders(self, s: list, vacancies: list):
+        """Форматирует границы таблицы по длине
+        Args:
+            s (list): Границы таблицы
+            vacancies (list): Список вакансий
+        Returns:
+            int, int: Границы таблицы
+        """
+        if len(s) == 0:
+            return 0, len(vacancies)
+        if len(s) == 1:
+            return s[0] - 1, len(vacancies)
+        return s[0] - 1, s[1] - 1
+
+    def formattingFields(self):
+        """Определяет, заданны ли колонки таблицы
+        Returns:
+            list: Выводимые колонки
+        """
+        if self.requiredColumns == ['']:
+            return self.vacanciesTable.field_names
+        self.requiredColumns.insert(0, "№")
+        return self.requiredColumns
+
+    def print_vacancies(self, data_vacancies: list):
+        """Печатает таблицу
+        Args:
+            data_vacancies (list): Список вакансий
+        """
+        self.vacanciesTable = prettytable.PrettyTable()
+        maxWidth = 20
+        self.vacanciesTable.field_names = ["№", "Название", "Описание", "Навыки", "Опыт работы", "Премиум-вакансия",
+                                           "Компания", "Оклад", "Название региона", "Дата публикации вакансии"]
+        self.vacanciesTable._max_width = {"№": 2, "Название": maxWidth, "Описание": maxWidth,
+                                          "Навыки": maxWidth, "Опыт работы": maxWidth, "Премиум-вакансия": maxWidth,
+                                          "Компания": maxWidth, "Оклад": maxWidth, "Название региона": maxWidth,
+                                          "Дата публикации вакансии": maxWidth}
+        self.vacanciesTable.align = "l"
+        self.vacanciesTable.hrules = prettytable.ALL
+        self.vacanciesTable.header = True
+        counter = 1
+        startingNumberVacancy, finalNumberVacancy = self.formattingBorders(self.borders, data_vacancies)
+        for vacancy in data_vacancies:
+            rows = [counter, vacancy.name, vacancy.description, vacancy.str_skills, vacancy.experience_id,
+                    vacancy.premium, vacancy.employer_name,
+                    f'{vacancy.salary.salary_from} - {vacancy.salary.salary_to} ({vacancy.salary.salary_currency}) ({vacancy.salary.salary_gross})',
+                    vacancy.area_name, vacancy.published_at.strftime("%d.%m.%Y")]
+            if self.isFilter == False:
+                counter += 1
+                self.vacanciesTable.add_row(rows)
+            else:
+                if self.parameterFilter[0] == 'Оклад':
+                    if int(vacancy.salary.salary_to.replace(' ', '')) >= int(self.parameterFilter[1]) and int(
+                            vacancy.salary.salary_from.replace(' ', '')) <= int(self.parameterFilter[1]):
+                        counter += 1
+                        self.vacanciesTable.add_row(rows)
+                elif self.parameterFilter[0] == 'Навыки':
+                    skills = self.parameterFilter[1]
+                    isEnterSkills = True
+                    skills = list(skills.split(", "))
+                    for skill in skills:
+                        if skill not in vacancy.key_skills:
+                            isEnterSkills = False
+                            break
+                    if isEnterSkills == True:
+                        counter += 1
+                        self.vacanciesTable.add_row(rows)
+                elif self.parameterFilter[0] == "Дата публикации вакансии":
+                    if vacancy.published_at.strftime("%d.%m.%Y") == self.parameterFilter[1]:
+                        counter += 1
+                        self.vacanciesTable.add_row(rows)
+                elif vacancy.returnsValues(self.parameterFilter[0]) == self.parameterFilter[1]:
+                    counter += 1
+                    self.vacanciesTable.add_row(rows)
+        if len(self.vacanciesTable.rows) != 0:
+            print(self.vacanciesTable.get_string(start=startingNumberVacancy, end=finalNumberVacancy,
+                                                 ﬁelds=self.formattingFields()))
+        else:
+            print("Ничего не найдено")
+
+
+currency_to_rub = {"Манаты": 35.68,
+                   "Белорусские рубли": 23.91,
+                   "Евро": 59.90,
+                   "Грузинский лари": 21.74,
+                   "Киргизский сом": 0.76,
+                   "Тенге": 0.13,
+                   "Рубли": 1,
+                   "Гривны": 1.64,
+                   "Доллары": 60.66,
+                   "Узбекский сум": 0.0055
+                   }
+dictTranslationExperience = {"noExperience": "Нет опыта",
+                             "between1And3": "От 1 года до 3 лет",
+                             "between3And6": "От 3 до 6 лет",
+                             "moreThan6": "Более 6 лет"}
+dictSortionExperience = {"Нет опыта": 1,
+                         "От 1 года до 3 лет": 2,
+                         "От 3 до 6 лет": 3,
+                         "Более 6 лет": 4}
+dictTranslationСurrency = {"AZN": "Манаты",
+                           "BYR": "Белорусские рубли",
+                           "EUR": "Евро",
+                           "GEL": "Грузинский лари",
+                           "KGS": "Киргизский сом",
+                           "KZT": "Тенге",
+                           "RUR": "Рубли",
+                           "UAH": "Гривны",
+                           "USD": "Доллары",
+                           "UZS": "Узбекский сум"}
+dictTranslationBool = {"True": "Да",
+                       "False": "Нет",
+                       "Да": True,
+                       "Нет": False,
+                       "FALSE": "Нет",
+                       "TRUE": "Да"}
+dictTranslationTaxes = {"False": "С вычетом налогов",
+                        "True": "Без вычета налогов",
+                        "FALSE": "С вычетом налогов",
+                        "TRUE": "Без вычета налогов"}
